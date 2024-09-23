4cb1c83a31ae43cc551977a5453902f475a69d81
davidbarren <davidbarrenechea98@gmail.com>
2024-09-23 22:28:22 +0300

    added ex02 with reverse iterators, still need to make sure it works properly

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarrene <dbarrene@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 21:55:14 by dbarrene          #+#    #+#             */
+/*   Updated: 2024/09/23 22:28:09 by dbarrene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include "MutantStack.hpp"
+int	main(void)
+{
+	std::cout << "rotta" << std::endl;
+
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+//	std::cout << mstack.top() << std::endl;
+//	mstack.pop();
+//	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+}
